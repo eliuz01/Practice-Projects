562b79620584a1b084bef0edea203a6557c61258
ELIUZ <osongoeliuz14@gmail.com>
2025-05-15 11:37:52 +0300

    create members template

--- myspace/my_rugby_club/members/views.py
+++ myspace/my_rugby_club/members/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 
-def members(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+def members(request):#function view
+    mymembers = Member.objects.all().values()#Query the Database and assign records to the created object 'mymembers' 
+    template = loader.get_template('all_members.html')#loads the template named all_members.html and assigns to object template
+    context = {#create dictionary context that stores data you want to pass into the template.
+        'mymembers' : mymembers
+    }
+    return HttpResponse(template.render(context, request))#ombines the dictionary and the template and returns them as an HttpResponse
