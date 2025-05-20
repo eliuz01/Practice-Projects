31beff51658328c16993cf786c98b23cc5f17d0d
ELIUZ <osongoeliuz14@gmail.com>
2025-05-20 12:51:32 +0300

    add a details page to list more details about a specific member

--- myspace/my_rugby_club/members/views.py
+++ myspace/my_rugby_club/members/views.py
@@ -8,6 +8,14 @@ def members(request):#function view
     mymembers = Member.objects.all().values()#Query the Database and assign records to the created object 'mymembers' 
     template = loader.get_template('all_members.html')#loads the template named all_members.html and assigns to object template
     context = {#create dictionary context that stores data you want to pass into the template.
-        'mymembers' : mymembers
+        'mymembers' : mymembers,
     }
     return HttpResponse(template.render(context, request))#ombines the dictionary and the template and returns them as an HttpResponse
+
+def details(request, id):
+    mymember = Member.objects.get(id=id)
+    template  = loader.get_template('details.html')
+    context = {
+        'mymember' : mymember,
+    }
+    return HttpResponse(template.render(context, request))
